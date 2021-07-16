61f1a041825d426cd55d3813a8cd03facc2a6b41
skyandy1891 <skyandy1891@gmail.com>
2021-07-16 20:07:03 +0300

    learn collections

---
+++ src/collections.js
@@ -0,0 +1,8 @@
+const colorPickerOptions = [
+  { label: "red", color: "#f44336" },
+  { label: "green", color: "#f4caf50" },
+  { label: "blue", color: "#2196f3" },
+  { label: "gray", color: "#607d8b" },
+  { label: "pink", color: "#e91e63" },
+  { label: "indigo", color: "#3f51b5" },
+];
--- src/creating-elements.js
+++ src/creating-elements.js
@@ -2,26 +2,32 @@ const listEl = document.querySelector(".cars");
 
 // console.log(listEl);
 // создаём элемент (заголовок)
-// const titleEl = document.createElement("h1");
+const titleEl = document.createElement("h1");
 
 // даём класс элементу
-// titleEl.classList.add("page-title");
+titleEl.classList.add("page-title");
 
 // даём заголовок элементу
-// titleEl.textContent = "Заголовок страницы";
+titleEl.textContent = "Заголовок страницы";
 
-// console.log(titleEl);
-// добавляем элемент на страницу
-// appendChild добавляет элемент в конец body
+// ===========================добавляем изображение в наш список элементов. опять же В КОНЕЦ!!!
 
-// document.body.appendChild(titleEl);
-
-// ============================добавляем изображение в наш список элементов. опять же В КОНЕЦ!!!
-// const imgEl = document.createElement("img");
-// imgEl.src = "https://www.goodfon.ru/wallpaper/devushka-leto-plyazh-2559.html";
-// imgEl.width = 480;
-// imgEl.alt = "beach";
+const imgEl = document.createElement("img");
+imgEl.src = "https://www.goodfon.ru/wallpaper/devushka-leto-plyazh-2559.html";
+imgEl.width = 480;
+imgEl.alt = "beach";
 // listEl.appendChild(imgEl);
+
+const heroEl = document.querySelector(".hero");
+
+// ============================добавили два элемента в hero
+
+// heroEl.appendChild(titleEl, imgEl);
+// heroEl.appendChild(imgEl);
+
+// ============================тоже самое, но за одну операцию!!!
+
+heroEl.append(titleEl, imgEl);
 
 // ============================добавляем пункты меню===============
 
@@ -31,7 +37,7 @@ navItemEl.classList.add("nav-item");
 const navLinkEl = document.createElement("a");
 navLinkEl.classList.add("nav-link");
 navLinkEl.textContent = "личный кабинет";
-navLinkEl.href = "/profile";
+navLinkEl.href = "/#";
 
 // вкладываем ссылку в li
 navItemEl.appendChild(navLinkEl);
@@ -48,9 +54,9 @@ listEl.insertBefore(navItemEl, listEl.firstElementChild);
 // добавляем li в перед последним элементом списка
 
 // insertBefore если нужно вставить перед последним... то lastElementChild
-listEl.insertBefore(navItemEl, listEl.lastElementChild);
+// listEl.insertBefore(navItemEl, listEl.lastElementChild);
 
 // добавляем li в перед любым элементом списка
 
 // с помощью children[index] можно поставить перед любым элементом
-listEl.insertBefore(navItemEl, listEl.children[1]);
+// listEl.insertBefore(navItemEl, listEl.children[1]);
