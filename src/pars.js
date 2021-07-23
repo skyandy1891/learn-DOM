23b225343a33affa8f74af3463a21fa583876cb6
skyandy1891 <skyandy1891@gmail.com>
2021-07-23 19:22:01 +0300

    ещё немного парса

--- src/pars.js
+++ src/pars.js
@@ -1,8 +1,23 @@
 const titleEl = document.querySelector(".title");
 // добавляем ссылку
-titleEl.innerHTML = "<a href=''>Это ссылка :)</a>";
+// titleEl.innerHTML = "<a href=''>Это ссылка :)</a>";
 
 // полностью очищает всё внутри данного элемента
 // titleEl.innerHTML = "";
 
-console.log(titleEl.innerHTML);
+// console.log(titleEl.innerHTML);
+
+// insertAdjacentHTML добавляет элемент не меняя внутри него ничего
+
+// добавляет элемент ПЕРЕД НАЧАЛОМ
+// titleEl.insertAdjacentHTML("beforebegin", "<a href=''>Это ссылка :)</a>");
+
+// добавляет элемент ПОСЛЕ, т.е. следующим соседом
+// titleEl.insertAdjacentHTML("afterend", "<a href=''>Это ссылка :)</a>");
+
+// добавит элемент ПЕРВЫМ ВНУТРИ
+// titleEl.insertAdjacentHTML("afterbegin", "<a href=''>Это ссылка :)</a>");
+
+// добавит элемент ПОСЛЕДНИМ ВНУТРИ
+
+// titleEl.insertAdjacentHTML("beforeend", "<a href=''>Это ссылка :)</a>");
